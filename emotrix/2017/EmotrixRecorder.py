9d20c479116b05cb92ab5ef663fe594a1fc00a13
Diego Jacobs <diegojacobs9595@gmail.com>
2017-09-13 17:38:45 -0600

    Emotrix Recorder

---
+++ emotrix/2017/EmotrixRecorder.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+# **********************************************************************************************************************
+# Archivo:      EmotrixRecorder.py
+# Proposito:    Este archivo es el encargado de leer la data directamente desde el EMOTIV, y guardarla en un archivo csv
+#               para luego hacer un preprocesamiento de ella.
+# Autor:        Diego Jacobs    13160
+# **********************************************************************************************************************
+
+import platform
+import csv
+import time
+import sys
+sys.path.insert(0, '/home/emotrix/Documents/EMOTRIX/emokit')
+
+from emotiv import Emotiv
+import gevent
+
+
+class EmotrixRecoder(object):
+
+    #El metodo init define algunos parametros por defecto, para almacenar las lecturas del EMOTIV
+    def __init__(self, fileName):
+        self.sequence = ['happy', 'neutral', 'sad', 'happy', 'neutral', 'sad', 'happy', 'neutral', 'sad', 'happy', 'neutral', 'sad','happy', 'neutral', 'sad','happy', 'neutral', 'sad','happy', 'neutral', 'sad']
+        self.time_block = 7
+        self.num_blocks = len(self.sequence)
+        self.filename = fileName
+
+    #Metodo para iniciar la grabacion de las lecturas de las señales EEG.
+    def start(self, sequence=None, sensors=None, time_block=7):
+        self.time_block = time_block
+        #Se define el objeto EMOTIV, utilizando la libreria EMOKIT
+        headset = Emotiv()
+        gevent.spawn(headset.setup)
+        
+        print("Serial Number: %s" % headset.serial_number)
+
+        if sequence is not None:
+            self.sequence = sequence
+            self.num_blocks = len(self.sequence)
+        i = 0
+        cont_block = 0
+        cont_seconds = 0
+        temp_t = 0
+        tag = self.sequence[0]
+
+        #Se define el escritor de las lecturas en el archivo CSV
+        writer = csv.writer(open(self.filename, 'w'), delimiter=',', quotechar='"')
+        try:
+            row = ["Time"]
+            for sensor in sensors:
+                row.append(sensor)
+                row.append(sensor + "_Quality")
+            row.append(tag)
+
+            writer.writerow(row)
+
+            t0 = time.time()
+            while True:
+                t = int(time.time()-t0)
+                
+                if temp_t != t:
+                    cont_seconds += 1
+
+                if cont_seconds > self.time_block:
+                    cont_seconds = 0
+                    cont_block += 1
+                    if cont_block == self.num_blocks:
+                        headset.close()
+                        break
+                    else:
+                        tag = self.sequence[cont_block]
+
+                # Se construye la informacion a guardar
+                row = [str(t)]
+                try:
+                    for sensor in sensors:
+                        row.append(str(headset.sensors[sensor]['value']))
+                        row.append(str(headset.sensors[sensor]['quality']))
+                    
+                except:
+                    print "Sensor incorrecto"
+                    headset.close()
+
+                row.append(tag)
+                # Se exporta a csv
+                writer.writerow(row)
+                print row
+                temp_t = t
+                gevent.sleep(0)
+        except KeyboardInterrupt:
+            headset.close()
+        finally:
+            headset.close()
+        i += 1
+
+
+#Se inicia el proceso de la grabacion, se define la secuencia y el tiempo de cada estimulo.
+er = EmotrixRecoder("data.csv")
+#Secuencia de imagenes, Sensores a leer, tiempo de corrida
+er.start(['NOPE','RELAX', 'RELAX', 'RELAX', 'RELAX',
+          'HAPPY', 'NEUTRAL', 'SAD', 'NEUTRAL',
+          'HAPPY', 'NEUTRAL', 'SAD', 'NEUTRAL',
+          'HAPPY', 'NEUTRAL', 'SAD', 'NEUTRAL',
+          'HAPPY', 'NEUTRAL', 'SAD', 'NEUTRAL',
+          'HAPPY', 'NEUTRAL', 'SAD', 'NEUTRAL',
+          'HAPPY', 'NEUTRAL', 'SAD', 'NEUTRAL',
+          'HAPPY', 'NEUTRAL', 'SAD', 'NEUTRAL',
+          'HAPPY', 'NEUTRAL', 'SAD', 'NEUTRAL',
+          'HAPPY', 'NEUTRAL', 'SAD', 'NEUTRAL',
+          'HAPPY', 'NEUTRAL', 'SAD', 'NEUTRAL',
+          ], ['F3', 'F4', 'AF3', 'AF4', 'O1', 'O2'], 4)
--- emotrix/2017/Reader.py
+++ emotrix/2017/Reader.py
@@ -1,9 +1,101 @@
+# -*- coding: utf-8 -*-
+# **********************************************************************************************************************
+# Archivo:      Reader.py
+# Proposito:    Este archivo es el encargado de leer los archivos csv
+# Autor:        Diego Jacobs    13160
+# **********************************************************************************************************************
+
 import csv
 import numpy as np
+from BlockData import BlockData
 
-class Reader (object):
+class Reader(object):
+    """
+    docstring here
+        :param object: 
+    """
     
-    def __init__(self, file):
-        self.fileName = file
+    def __init__(self, fileData, tagsToIgnore):
+        """
+        docstring here
+            :param self: 
+            :param fileData: 
+            :param tagsToIgnore: 
+        """
+        self.fileName = fileData
+        self.ignoreTags = tagsToIgnore
         self.time = np.array([])
+        self.f3 = np.array([])
+        self.f4 = np.array([])
+        self.af3 = np.array([])
+        self.af4 = np.array([])
+        self.tag = np.array([])
+        self.blocks = []
+
+    def readFile(self):
+        """
+        docstring here
+            :param self: 
+        """
+        try:
+            fileToRead = open(self.fileName, "rb")
+            reader = csv.reader(fileToRead, delimiter='\t', quotechar='"')
+
+            time, f3, f4, af3, af4, tag = [], [], [], [], [], []
+            for row in reader:
+                if len(row) == 6:
+                    if row[5] in self.ignoreTags:
+                        continue
+                    f3_value = int(row[1].split(':')[1].split(',')[1])
+                    f3_quality = int(row[1].split(':')[1].split(',')[0])
+                    f4_value = int(row[2].split(':')[1].split(',')[1])
+                    f4_quality = int(row[2].split(':')[1].split(',')[0])
+                    af3_value = int(row[3].split(':')[1].split(',')[1])
+                    af3_quality = int(row[3].split(':')[1].split(',')[0])
+                    af4_value = int(row[4].split(':')[1].split(',')[1])
+                    af4_quality = int(row[4].split(':')[1].split(',')[0])
+                    #Filtro
+                    if min(f3_quality, f4_quality, af3_quality, af4_quality) >= 1:
+                        time.append(int(row[0]))
+                        f3.append(f3_value)
+                        f4.append(f4_value)
+                        af3.append(af3_value)
+                        af4.append(af4_value)
+                        tag.append(row[5])
+        except:
+            print 'No encontrado: ' + self.fileName + '.csv'
+            fileToRead.close()
+
+        #Los arreglos se transforman a arreglos de numpy, para que sea mas eficiente.
+        self.time = np.append(self.time, np.array(time))
+        self.tag = np.append(self.tag, np.array(tag))
+        self.f3 = np.append(self.f3, np.array(f3))
+        self.f4 = np.append(self.f4, np.array(f4))
+        self.af3 = np.append(self.af3, np.array(af3))
+        self.af4 = np.append(self.af4, np.array(af4))
+
         
+    #Metodo para obtener los datos en bloques de 1 segundo.
+    def getBlocksData(self):
+        self.blocks = []
+        before = self.tag[0]
+        lower_limit_i = 0
+        i = 0
+        size = 256
+        overlap = 20
+        for t in np.append(self.tag, 'fin'):
+            if t != before:
+                previous_second = self.time[lower_limit_i]
+                for j in range(lower_limit_i, i-128, overlap):
+                    #Se descarta el primer segundo de cada bloque
+                    if j != lower_limit_i:
+                        block = BlockData(tag=before, time=self.time[j], f3=self.f3[j:j+size], f4=self.f4[j:j+size], af3=self.af3[j:j+size], af4=self.af4[j:j+size])
+                        self.blocks.append(block)
+                lower_limit_i = i
+                before = t
+            i += 1
+        return self.blocks
+
+r = Reader("/home/emotrix/Documents/EMOTRIX/emotrix/2017/user2.csv", ["RELAX", "NOPE"])
+r.readFile()
+print r.getBlocksData()
