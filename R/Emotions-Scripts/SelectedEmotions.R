c2f85befacdf9df64e93b3a5a28f0f55736fde54
Diego Jacobs <diegojacobs9595@gmail.com>
2017-10-12 20:21:35 -0600

    Wavelets

--- R/Emotions-Scripts/SelectedEmotions.R
+++ R/Emotions-Scripts/SelectedEmotions.R
@@ -5,21 +5,31 @@ install.packages("data.table")
 library(readr)
 library(data.table)
 
-setwd("~/Documents/Emotrix/emotrix/2017/Data")
+setwd("D:/Diego Jacobs/Documents/Emotrix/emotrix/2017/Data")
 
 folder <- "Selected-Emotions"
 
-files <- list.files(path="~/Documents/Emotrix/emotrix/2017/Data", pattern="csv$", full.names=FALSE, recursive=FALSE)
+files <- list.files(path="D:/Diego Jacobs/Documents/Emotrix/emotrix/2017/Data", pattern="csv$", full.names=FALSE, recursive=FALSE)
 
 for(i in 1:length(files)){
   csv <- read_csv(files[i], col_types = cols(`Exact Time` = col_double()))
   DT <- data.table(csv)
   
-  images <-unique(DT[Emotion != "NON-RELAX"  & Emotion != "RELAX", c("Image/Color", "Time")])
-  emotions <- DT[Emotion != "NON-RELAX"  & Emotion != "RELAX" & `Selected Emotion` != "NA"]
-  table <- emotions[, c("Image/Color", "Time", "Emotion", "Selected Emotion")]
+  if("Image/Color" %in% colnames(DT)){
+    tuple <-unique(DT[Emotion != "NON-RELAX"  & Emotion != "RELAX", c("Image/Color", "Time")])
+  } else {
+    tuple <-unique(DT[Emotion != "NON-RELAX"  & Emotion != "RELAX", c("Audio", "Time")])
+  }
   
-  final_table <- merge(images, table, all.x=TRUE)
+  emotions <- DT[Emotion != "NON-RELAX"  & Emotion != "RELAX" & `Selected Emotion` != "NA"]
+  
+  if("Image/Color" %in% colnames(DT)){
+    table <- emotions[, c("Image/Color", "Time", "Emotion", "Selected Emotion")]
+  } else {
+    table <- emotions[, c("Audio", "Time", "Emotion", "Selected Emotion")]
+  }
+  
+  final_table <- merge(tuple, table, all.x=TRUE)
   
   table_order <- unique(final_table[order(final_table$Time)])
   
@@ -29,8 +39,10 @@ for(i in 1:length(files)){
 
 remove(csv)
 remove(DT)
-remove(images)
+remove(tuple)
 remove(emotions)
 remove(table)
 remove(final_table)
 remove(table_order)
+remove(folder)
+remove(files)
--- R/wavelets.R
+++ R/wavelets.R
@@ -1,31 +1,49 @@
 install.packages("readr")
 install.packages("wavelets")
 install.packages("data.table")
+install.packages("signal")
 library(readr)
 library(wavelets)
 library(data.table)
+library(signal)
 
 setwd("D:/Diego Jacobs/Documents/Emotrix/emotrix/2017/Data")
 
 
-csv <- read_csv("17F2142.csv", col_types = cols(`Exact Time` = col_double()))
-csv2 <- csv[(csv$Emotion == "NON-RELAX" & csv$Emotion == "RELAX"),]
+csv <- read_csv("17M2311.csv", col_types = cols(`Exact Time` = col_double()))
+csv2 <- csv[(csv$Emotion != "NON-RELAX" & csv$Emotion != "RELAX"),]
 
-m_f3 <- as.matrix(csv2$F3)
-m_af3 <- as.matrix(csv2$AF3)
-m_af4 <- as.matrix(csv2$AF4)
-m_f4 <- as.matrix(csv2$F4)
-m_o1 <- as.matrix(csv2$O2)
-m_o2 <- as.matrix(csv2$O1)
+#Grafica de la lectura de un electrodo
+csv2 <- csv[(csv$Emotion != "NON-RELAX" & csv$Emotion != "RELAX"),]
+DT <- data.table(csv2)
+graph <- DT[, list(cantidad = mean(F3)), by='Time']
+plot(graph$Time, graph$intensidad, type='l')
+
+#Cantidad de valores distintos por segundo
+csv2 <- csv[(csv$Emotion != "NON-RELAX" & csv$Emotion != "RELAX"),]
+DT <- data.table(csv2)
+graph <- DT[, list(cantidad = length(unique(F3))), by='Time']
+
+
+m_f3 <- as.matrix(csv3$F3)
+m_af3 <- as.matrix(csv3$AF3)
+m_af4 <- as.matrix(csv3$AF4)
+m_f4 <- as.matrix(csv3$F4)
+m_o1 <- as.matrix(csv3$O2)
+m_o2 <- as.matrix(csv3$O1)
 
 m <- cbind(m_f3, m_af3, m_f4, m_af4, m_o1, m_o2)
 
-wt <- dwt(as.numeric(m), "d4", n.levels=3, boundary="periodic", fast=FALSE)
-
-View(wt@V$V3)
+wt <- dwt(as.numeric(m), filter='d4', n.levels=4, boundary="periodic", fast=FALSE)
 
 remove(csv)
 remove(csv2)
 remove(m_f3)
-remove(array)
+remove(m)
 remove(wt)
+remove(m_f3)
+remove(m_af3)
+remove(m_f4)
+remove(m_af4)
+remove(m_o1)
+remove(m_o2)
