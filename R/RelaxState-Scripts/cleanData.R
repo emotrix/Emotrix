bbc9ee07a421a8cf2024398213f4db7299f7795d
Mario Barrientos <mario_b_a03@hotmail.com>
2017-10-18 12:55:01 -0600

    relax state graphics

--- R/RelaxState-Scripts/cleanData.R
+++ R/RelaxState-Scripts/cleanData.R
@@ -3,16 +3,31 @@
 #Mario Barrientos - 13039
 
 #Librerias
+
+install.packages("readr")
+install.packages("data.table")
+
 library(readr)
+library(data.table)
 
-setwd("C:/Users/mario/Desktop/resultados")
-# setwd("C:/Users/mario/Desktop/DATAFIX")
-
-X17F1817 <- read_csv("C:/Users/mario/Desktop/resultados/17F1817.csv")
-sf1 <- X17F1817[(X17F1817$Emotion == "NON-RELAX" | X17F1817$Emotion == "RELAX"),]
-sf1$`Selected Emotion` <- NULL
-remove(X17F1817)
-sf1 <- unique(sf1)
-sf1$Teorico <- ifelse(sf1$Emotion == "NON-RELAX",0,1)
+setwd("C:/Users/mario/Desktop/Emotrix/allData")
 
 
+files <- list.files(path="C:/Users/mario/Desktop/Emotrix/allData", pattern="csv$", full.names=FALSE, recursive=FALSE)
+folder <- "../cleanData"
+
+for(i in 1:length(files)){
+  csv <- read_csv(files[i], col_types = cols(`Exact Time` = col_double()))
+  csv$`Selected Emotion`<-NULL
+  csv$Teorico <- ifelse(csv$Emotion == "NON-RELAX",0,1)
+  DT <- data.table(unique(csv[(csv$Emotion == "NON-RELAX" | csv$Emotion == "RELAX"),]))
+  
+  if("Image/Color" %in% colnames(DT)){
+    setnames(DT,"Image/Color","Image/Audio")
+  } else {
+    setnames(DT,"Audio","Image/Audio")
+  }
+  
+  dir.create(folder, showWarnings = FALSE)
+  write.csv(DT, file.path(folder, files[i]), row.names=FALSE)
+}
